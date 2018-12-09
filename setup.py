cd05587b68f1aeffe66dda56b9cfda7dbbcf8c3c
Katherine Crowson <crowsonkb@gmail.com>
2018-12-09 06:40:30 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,16 +3,18 @@ from setuptools import setup
 setup(
     name='pharmacokinetics',
     version='0.1.0',
-    description='Calculates and plots drug concentration over time.',
+    description='A Flask web application to calculate and plot drug concentration over time.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     url='https://github.com/crowsonkb/pharmacokinetics2',
     author='Katherine Crowson',
     author_email='crowsonkb@gmail.com',
     license='MIT',
-    packages=['pk'],
+    packages=['pk', 'pk_webapp'],
     install_requires=[s.strip() for s in open('requirements.txt').readlines()],
     entry_points={
         'console_scripts': ['pk=pk.cli:main'],
     },
+    include_package_data=True,
+    zip_safe=False,
 )
