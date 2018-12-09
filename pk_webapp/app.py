a2f42119af7b9ec43dc0ff59851b7450b24bd409
Katherine Crowson <crowsonkb@gmail.com>
2018-12-09 06:07:49 -0800

    Allow the chart to be downloaded in CSV format

--- pk_webapp/app.py
+++ pk_webapp/app.py
@@ -24,23 +24,38 @@ def root():
 
 class Concentration:
     def __init__(self, **kwargs):
-        self.hl = float(kwargs['hl'])
+        self.hl_e = float(kwargs['hl'])
         self.t_max = float(kwargs['t-max'])
         self.duration = float(kwargs['duration'])
         dose_qs = map(float, kwargs['doses'].split())
         offsets = map(float, kwargs['offsets'].split())
         self.doses = dict(zip(offsets, dose_qs))
-        drug = pk.Drug(self.hl, self.t_max)
+        self.drug = pk.Drug(self.hl_e, self.t_max)
         self.steps = 60
         self.num = round(self.duration * self.steps + 1)
         self.x = np.arange(self.num) / self.steps
-        self.y = drug.concentration(self.num, 1/self.steps, self.doses)
+        self.y = self.drug.concentration(self.num, 1/self.steps, self.doses)
+
+
+@app.route('/concentration.csv')
+def concentration_csv():
+    conc = Concentration(**request.args.to_dict())
+    x = np.c_[conc.x, conc.y]
+    buf = io.BytesIO()
+    np.savetxt(buf, x, delimiter=',', header='hours,concentration', comments='')
+    return Response(buf.getvalue(), mimetype='text/csv')
 
 
 @app.route('/concentration.json')
 def concentration_json():
     conc = Concentration(**request.args.to_dict())
-    msg = {'concentration': list(conc.y), 'steps': conc.steps}
+    msg = {'concentration': list(conc.y),
+           'drug': {'c_0': conc.drug.c_0,
+                    'hl_a': conc.drug.hl_a,
+                    'hl_e': conc.drug.hl_e,
+                    't_max': conc.drug.t_max},
+           'doses': conc.doses,
+           'steps': conc.steps}
     return jsonify(msg)
 
 
--- pk_webapp/static/main.js
+++ pk_webapp/static/main.js
@@ -4,8 +4,13 @@ $(document).ready(() => {
     $("#main-form").submit(e => {
         e.preventDefault();
         let dummy = new Date().getTime();
-        let query = "concentration.svg?" + $("#main-form").serialize() + "&dummy=" + dummy;
-        let img = $("<img>", {id: "chart", src: query});
-        $("#chart-container").html(img);
+        let query = `?${$("#main-form").serialize()}&dummy=${dummy}`;
+        let img = $("<img>", {id: "chart", src: "concentration.svg" + query});
+        let csvLink = $("<a>", {
+            class: "csv-download",
+            href: "concentration.csv" + query,
+            text: "Download data as CSV"
+        });
+        $("#chart-container").html([csvLink, "<br>", img]);
     });
 });
